9b82edb4e89f2528a60c0d919d16d4d245746175
Mohammad Sianaki <m.rsianaki@yahoo.com>
2020-01-29 16:56:21 +0330

    Add credential values

--- app/src/main/java/com/mydigipay/challenge/github/MainActivity.kt
+++ app/src/main/java/com/mydigipay/challenge/github/MainActivity.kt
@@ -6,9 +6,9 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
-const val CLIENT_ID = "CLIENT_ID"
-const val CLIENT_SECRET = "CLIENT_SECRET"
-const val REDIRECT_URI = "REDIRECT_URI"
+const val CLIENT_ID = "Iv1.551bf597c857ddf9"
+const val CLIENT_SECRET = "0ca09241dd2b5b9d6aa4d7d8affe1f617f45fc91"
+const val REDIRECT_URI = ""
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
