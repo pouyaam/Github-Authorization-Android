36f5fdb34096ee18691d74f59c45036bcf1ebc7c
Mohammad Sianaki <m.rsianaki@yahoo.com>
2020-01-30 18:15:21 +0330

    Fix runtime exception

--- app/src/main/java/com/mydigipay/challenge/data/api/github/GithubService.kt
+++ app/src/main/java/com/mydigipay/challenge/data/api/github/GithubService.kt
@@ -2,9 +2,10 @@ package com.mydigipay.challenge.data.api.github
 
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface GithubService {
 
     @GET("/search/repositories")
-    fun performSearch(query: String): Call<SearchResponseDto>
+    fun performSearch(@Query("q") query: String): Call<SearchResponseDto>
 }
