a40e735f44dc0a7b3e557799efe02772cb32cd94
Mohammad Sianaki <m.rsianaki@yahoo.com>
2020-01-31 13:59:36 +0330

    Add detailViewModel to viewModelModule for di purpose

--- app/src/main/java/com/mydigipay/challenge/di/ViewModelModule.kt
+++ app/src/main/java/com/mydigipay/challenge/di/ViewModelModule.kt
@@ -1,17 +1,15 @@
 package com.mydigipay.challenge.di
 
 import com.mydigipay.challenge.presentation.ui.MainViewModel
-import com.mydigipay.challenge.presentation.ui.github.search.SearchViewModel
 import com.mydigipay.challenge.presentation.ui.github.GithubiViewModel
+import com.mydigipay.challenge.presentation.ui.github.detail.DetailViewModel
+import com.mydigipay.challenge.presentation.ui.github.search.SearchViewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 val viewModelModule = module {
-    viewModel {
-        GithubiViewModel(
-            get()
-        )
-    }
+    viewModel { GithubiViewModel(get()) }
     viewModel { SearchViewModel(get()) }
     viewModel { MainViewModel(get()) }
+    viewModel { DetailViewModel(get()) }
 }
