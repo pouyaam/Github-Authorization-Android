e9721fa7aa9f8bb8657adf9966a2f8704ac25106
TurKurT656 <turkurt656@gmail.com>
2020-03-03 14:20:05 +0330

    added ktx functions to observe events

---
+++ app/src/main/java/com/mydigipay/challenge/util/livedata/Event.kt
@@ -0,0 +1,85 @@
+package com.mydigipay.challenge.util.livedata
+
+import androidx.annotation.MainThread
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Observer
+
+/**
+ * Used as a wrapper for data that is exposed via a LiveData that represents an event.
+ */
+open class Event<out T>(private val content: T) {
+
+    private var consumed = false
+
+    /**
+     * Returns the content and prevents its use again.
+     */
+    fun consume(): T? {
+        return if (consumed) {
+            null
+        } else {
+            consumed = true
+            content
+        }
+    }
+
+    /**
+     * Returns the content, even if it's already been handled.
+     */
+    fun peek(): T = content
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Event<*>
+
+        if (content != other.content) return false
+        if (consumed != other.consumed) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = content?.hashCode() ?: 0
+        result = 31 * result + consumed.hashCode()
+        return result
+    }
+}
+
+class StateLessEvent : Event<Any>("")
+
+
+/**
+ * An [Observer] for [Event]s, simplifying the pattern of checking if the [Event]'s content has
+ * already been consumed.
+ *
+ * [onEventUnconsumedContent] is *only* called if the [Event]'s contents has not been consumed.
+ */
+class EventObserver<T>(private val onEventUnconsumedContent: (T) -> Unit) : Observer<Event<T>> {
+    override fun onChanged(event: Event<T>?) {
+        event?.consume()?.run(onEventUnconsumedContent)
+    }
+}
+
+@MainThread
+inline fun <T> LiveData<Event<T>>.observeEvent(
+    owner: LifecycleOwner,
+    crossinline onChanged: (T) -> Unit
+): EventObserver<T> {
+    return EventObserver<T> { t -> onChanged.invoke(t) }.apply {
+        observe(owner, this)
+    }
+}
+
+
+@MainThread
+inline fun LiveData<StateLessEvent>.observeEvent(
+    owner: LifecycleOwner,
+    crossinline onChanged: () -> Unit
+): EventObserver<Any> {
+    return EventObserver<Any> { onChanged.invoke() }.apply {
+        observe(owner, this)
+    }
+}
