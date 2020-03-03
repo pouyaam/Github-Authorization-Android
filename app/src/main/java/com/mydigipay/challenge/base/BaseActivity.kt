92806712a2dbd6d37b83e2352358d0e504c8a44c
TurKurT656 <turkurt656@gmail.com>
2020-03-03 13:17:05 +0330

    created base activity

---
+++ app/src/main/java/com/mydigipay/challenge/base/BaseActivity.kt
@@ -0,0 +1,46 @@
+package com.mydigipay.challenge.base
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+
+abstract class BaseActivity<VM : BaseViewModel, DB : ViewDataBinding> : AppCompatActivity() {
+
+    abstract val viewModel: VM
+
+    abstract val layoutRes: Int
+
+    abstract val navigationId: Int
+
+    val binding by lazy {
+        DataBindingUtil.setContentView(this, layoutRes) as DB
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        initBinding()
+        bindObservables()
+        oneTimeEvent()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        everyTimeEvent()
+    }
+
+    /**
+     *
+     *  You need override this method.
+     *  And you need to set viewModel to binding: binding.viewModel = viewModel
+     *
+     */
+    abstract fun initBinding()
+
+    abstract fun bindObservables()
+
+    abstract fun oneTimeEvent()
+
+    abstract fun everyTimeEvent()
+
+}
