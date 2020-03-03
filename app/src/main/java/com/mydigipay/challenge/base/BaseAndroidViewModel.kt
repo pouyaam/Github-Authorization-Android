38f61bb41b700617f932a1bd3ff10076738776ee
TurKurT656 <turkurt656@gmail.com>
2020-03-03 13:15:52 +0330

    added getString method for base android vm as a sample extra func

--- app/src/main/java/com/mydigipay/challenge/base/BaseAndroidViewModel.kt
+++ app/src/main/java/com/mydigipay/challenge/base/BaseAndroidViewModel.kt
@@ -1,6 +1,7 @@
 package com.mydigipay.challenge.base
 
 import android.app.Application
+import androidx.annotation.StringRes
 import com.mydigipay.challenge.app.App
 
 abstract class BaseAndroidViewModel(
@@ -8,5 +9,9 @@ abstract class BaseAndroidViewModel(
 ) : BaseViewModel() {
 
     protected val app = application as App
+
+    protected fun getString(@StringRes stringRes: Int, vararg formatArgs: Any?): String {
+        return app.resources.getString(stringRes, *formatArgs)
+    }
 
 }
