e1ceb98f1c6ca28ee1fdd25a8cdf6f547a139f8b
TurKurT656 <turkurt656@gmail.com>
2020-03-03 19:00:36 +0330

    changed client id, client secret and redirect url

--- app/src/main/java/com/mydigipay/challenge/github/MainActivity.kt
+++ app/src/main/java/com/mydigipay/challenge/github/MainActivity.kt
@@ -6,9 +6,9 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
-const val CLIENT_ID = "CLIENT_ID"
-const val CLIENT_SECRET = "CLIENT_SECRET"
-const val REDIRECT_URI = "REDIRECT_URI"
+const val CLIENT_ID = "31d8fee088f5e2caed71"
+const val CLIENT_SECRET = "ba1e3bc0669edb06c928d9233d7ce775d87a787c"
+const val REDIRECT_URI = "challenge://mydigipay.com/login/callback"
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
