c3813208c99c5408e62eb4dce1e5a9dfd6c07ddd
TurKurT656 <turkurt656@gmail.com>
2020-03-04 01:25:36 +0330

    added button to open login page via intent

--- app/src/main/java/com/mydigipay/challenge/di/ViewModelModule.kt
+++ app/src/main/java/com/mydigipay/challenge/di/ViewModelModule.kt
@@ -7,7 +7,7 @@ import org.koin.dsl.module
 
 val viewModelModule = module {
 
-    viewModel { LoginViewModel() }
+    viewModel { LoginViewModel(get()) }
     viewModel { HomeViewModel() }
 
 }
--- app/src/main/java/com/mydigipay/challenge/ui/login/LoginViewModel.kt
+++ app/src/main/java/com/mydigipay/challenge/ui/login/LoginViewModel.kt
@@ -1,5 +1,16 @@
 package com.mydigipay.challenge.ui.login
 
+import android.content.Intent
+import android.net.Uri
 import com.mydigipay.challenge.base.BaseViewModel
+import com.mydigipay.challenge.data.repository.token.TokenRepository
 
-class LoginViewModel : BaseViewModel()
+class LoginViewModel(
+    private val tokenRepository: TokenRepository
+) : BaseViewModel() {
+
+    fun onLoginClicked() {
+        openIntent(action = Intent.ACTION_VIEW, uri = Uri.parse(tokenRepository.getLoginUrl()))
+    }
+
+}
