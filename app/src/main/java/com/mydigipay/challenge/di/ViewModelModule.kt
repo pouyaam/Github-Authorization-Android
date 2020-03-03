aed82c2e775ca0ba70d8a4eaf47d1f19a6970663
TurKurT656 <turkurt656@gmail.com>
2020-03-04 00:31:36 +0330

    added vm module to the di

---
+++ app/src/main/java/com/mydigipay/challenge/di/ViewModelModule.kt
@@ -0,0 +1,13 @@
+package com.mydigipay.challenge.di
+
+import com.mydigipay.challenge.ui.home.HomeViewModel
+import com.mydigipay.challenge.ui.login.LoginViewModel
+import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.dsl.module
+
+val viewModelModule = module {
+
+    viewModel { LoginViewModel() }
+    viewModel { HomeViewModel() }
+
+}
