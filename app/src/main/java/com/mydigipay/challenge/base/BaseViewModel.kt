b8c424cbe058978eea307f5150e2af67d3cfe6b8
TurKurT656 <turkurt656@gmail.com>
2020-03-03 14:20:47 +0330

    added functions to be able navigate inside vm

--- app/src/main/java/com/mydigipay/challenge/base/BaseViewModel.kt
+++ app/src/main/java/com/mydigipay/challenge/base/BaseViewModel.kt
@@ -1,7 +1,40 @@
 package com.mydigipay.challenge.base
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.navigation.NavDirections
+import com.mydigipay.challenge.util.NavigationCommand
+import com.mydigipay.challenge.util.livedata.Event
 
 abstract class BaseViewModel() : ViewModel() {
+
+    /** Used in situations when we need to navigate to another fragment/activity
+     */
+    private val _navigationCommand = MutableLiveData<Event<NavigationCommand>>()
+    val navigationCommand: LiveData<Event<NavigationCommand>>
+        get() = _navigationCommand
+
+
+    /**
+     *      getting directions from the navigation's built actions
+     */
+    fun navigateTo(directions: NavDirections) {
+        _navigationCommand.postValue(Event(NavigationCommand.To(directions)))
+    }
+
+    /**
+     * using [NavController][androidx.navigation.NavController]'s navigateUp()
+     */
+    fun navigateBack() {
+        _navigationCommand.postValue(Event(NavigationCommand.Back))
+    }
+
+    /**
+     * using [NavController][androidx.navigation.NavController]'s popBackStack()
+     */
+    fun navigateBackTo(destinationId: Int, inclusive: Boolean) {
+        _navigationCommand.postValue(Event(NavigationCommand.BackTo(destinationId, inclusive)))
+    }
 
 }
