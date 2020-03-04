61bdf3fbbd0a42c8e1e066184134f15c9a5d0786
TurKurT656 <turkurt656@gmail.com>
2020-03-04 17:23:58 +0330

    injected vm via koin

--- app/src/main/java/com/mydigipay/challenge/di/ViewModelModule.kt
+++ app/src/main/java/com/mydigipay/challenge/di/ViewModelModule.kt
@@ -2,6 +2,7 @@ package com.mydigipay.challenge.di
 
 import com.mydigipay.challenge.ui.home.HomeViewModel
 import com.mydigipay.challenge.ui.login.LoginViewModel
+import com.mydigipay.challenge.ui.splash.SplashViewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
@@ -9,5 +10,6 @@ val viewModelModule = module {
 
     viewModel { LoginViewModel(get()) }
     viewModel { HomeViewModel() }
+    viewModel { SplashViewModel(get()) }
 
 }
