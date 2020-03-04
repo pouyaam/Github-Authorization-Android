7c7668c7a8e7585e4b45d19d332c97b1b43c4b64
TurKurT656 <turkurt656@gmail.com>
2020-03-04 23:25:00 +0330

    added rv in the repo list fragment

--- app/src/main/java/com/mydigipay/challenge/ui/repository/list/RepositoryListFragment.kt
+++ app/src/main/java/com/mydigipay/challenge/ui/repository/list/RepositoryListFragment.kt
@@ -3,6 +3,8 @@ package com.mydigipay.challenge.ui.repository.list
 import com.mydigipay.challenge.R
 import com.mydigipay.challenge.base.BaseFragment
 import com.mydigipay.challenge.databinding.FragmentRipositoryListBinding
+import kotlinx.android.synthetic.main.fragment_ripository_list.*
+import org.koin.android.ext.android.inject
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class RepositoryListFragment :
@@ -10,6 +12,11 @@ class RepositoryListFragment :
 
     override val viewModel: RepositoryListViewModel by viewModel()
     override val layoutRes: Int = R.layout.fragment_ripository_list
+    private val adapter: GitRepoAdapter by inject()
+
+    override fun oneTimeEvent() {
+        repo_list.adapter = adapter
+    }
 
     override fun initBinding() {
         binding?.apply {
