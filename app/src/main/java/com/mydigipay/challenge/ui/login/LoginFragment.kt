b766d3182a6294d7d8f6b259e798c08e1cf0c278
TurKurT656 <turkurt656@gmail.com>
2020-03-04 13:53:13 +0330

    added logic of getting access token from code

--- app/src/main/java/com/mydigipay/challenge/ui/login/LoginFragment.kt
+++ app/src/main/java/com/mydigipay/challenge/ui/login/LoginFragment.kt
@@ -10,6 +10,11 @@ class LoginFragment : BaseFragment<LoginViewModel, FragmentLoginBinding>() {
     override val viewModel: LoginViewModel by viewModel()
     override val layoutRes: Int = R.layout.fragment_login
 
+    override fun everyTimeEvent() {
+        val code = activity?.intent?.data?.getQueryParameter("code")
+        viewModel.setCode(code)
+    }
+
     override fun initBinding() {
         binding?.apply {
             lifecycleOwner = viewLifecycleOwner
--- app/src/main/java/com/mydigipay/challenge/ui/login/LoginViewModel.kt
+++ app/src/main/java/com/mydigipay/challenge/ui/login/LoginViewModel.kt
@@ -2,15 +2,44 @@ package com.mydigipay.challenge.ui.login
 
 import android.content.Intent
 import android.net.Uri
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.mydigipay.challenge.R
 import com.mydigipay.challenge.base.BaseViewModel
+import com.mydigipay.challenge.data.network.ApiResult
 import com.mydigipay.challenge.data.repository.token.TokenRepository
+import com.mydigipay.challenge.util.ktx.launch
 
 class LoginViewModel(
     private val tokenRepository: TokenRepository
 ) : BaseViewModel() {
 
+    private val code = MutableLiveData<String>()
+
+    private val _isLoading = MutableLiveData<Boolean>()
+    val isLoading: LiveData<Boolean>
+        get() = _isLoading
+
     fun onLoginClicked() {
         openIntent(action = Intent.ACTION_VIEW, uri = Uri.parse(tokenRepository.getLoginUrl()))
+    }
+
+
+    fun setCode(code: String?) {
+        this.code.postValue(code)
+        getToken(code)
+    }
+
+    private fun getToken(code: String?) = launch {
+        _isLoading.postValue(true)
+        code.takeIf { !it.isNullOrBlank() }?.let { code ->
+            when (val result = tokenRepository.accessToken(code)) {
+                is ApiResult.Success -> showToast(R.string.login_successful)
+                is ApiResult.Error -> showToast(result.message)
+            }
+        }
+        _isLoading.postValue(false)
+
     }
 
 }
