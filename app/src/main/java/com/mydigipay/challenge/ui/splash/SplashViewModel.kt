426bd31660767329e6bc1adbe516b980dec898af
TurKurT656 <turkurt656@gmail.com>
2020-03-04 23:19:19 +0330

    added navigation to repo list

--- app/src/main/java/com/mydigipay/challenge/ui/splash/SplashViewModel.kt
+++ app/src/main/java/com/mydigipay/challenge/ui/splash/SplashViewModel.kt
@@ -1,6 +1,5 @@
 package com.mydigipay.challenge.ui.splash
 
-import com.mydigipay.challenge.R
 import com.mydigipay.challenge.base.BaseViewModel
 import com.mydigipay.challenge.data.repository.token.TokenRepository
 import com.mydigipay.challenge.util.ktx.launch
@@ -14,8 +13,10 @@ class SplashViewModel(
     }
 
     private fun checkDestination() = launch {
-        if (tokenRepository.isUserLoggedIn()) showSnackBar(R.string.login_successful)
-        else navigateTo(SplashFragmentDirections.actionSplashToLogin())
+        if (tokenRepository.isUserLoggedIn())
+            navigateTo(SplashFragmentDirections.actionSplashToRepositories())
+        else
+            navigateTo(SplashFragmentDirections.actionSplashToLogin())
     }
 
 }
