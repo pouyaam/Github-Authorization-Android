66edcef80af8ac18218349c89c304ae954a7f27e
TurKurT656 <turkurt656@gmail.com>
2020-03-05 21:52:38 +0330

    completed logic of the commit list

--- app/src/main/java/com/mydigipay/challenge/ui/repository/detail/RepositoryDetailFragment.kt
+++ app/src/main/java/com/mydigipay/challenge/ui/repository/detail/RepositoryDetailFragment.kt
@@ -4,6 +4,9 @@ import androidx.navigation.fragment.navArgs
 import com.mydigipay.challenge.R
 import com.mydigipay.challenge.base.BaseFragment
 import com.mydigipay.challenge.databinding.FragmentRepositoryDetailBinding
+import com.mydigipay.challenge.util.EndlessRecyclerViewScrollListener
+import kotlinx.android.synthetic.main.fragment_repository_detail.*
+import org.koin.android.ext.android.inject
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class RepositoryDetailFragment : BaseFragment<RepositoryDetailViewModel, FragmentRepositoryDetailBinding>() {
@@ -11,9 +14,29 @@ class RepositoryDetailFragment : BaseFragment<RepositoryDetailViewModel, Fragmen
     override val viewModel: RepositoryDetailViewModel by viewModel()
     override val layoutRes: Int = R.layout.fragment_repository_detail
     private val args: RepositoryDetailFragmentArgs by navArgs()
+    private val adapter: CommitAdapter by inject()
+
+    private var endlessScroller: EndlessRecyclerViewScrollListener? = null
 
     override fun oneTimeEvent() {
         viewModel.setGitRepo(args.gitRepo)
+        configList()
+    }
+
+    private fun configList() {
+        commit_list?.apply {
+            adapter = this@RepositoryDetailFragment.adapter
+            layoutManager?.let { layoutManager ->
+                endlessScroller =
+                    object : EndlessRecyclerViewScrollListener(layoutManager) {
+                        override fun onLoadMore(page: Int) {
+                            viewModel.getCommits(page)
+                        }
+                    }.also { endlessScroller ->
+                        addOnScrollListener(endlessScroller)
+                    }
+            }
+        }
     }
 
     override fun initBinding() {
--- app/src/main/java/com/mydigipay/challenge/ui/repository/detail/RepositoryDetailViewModel.kt
+++ app/src/main/java/com/mydigipay/challenge/ui/repository/detail/RepositoryDetailViewModel.kt
@@ -3,16 +3,47 @@ package com.mydigipay.challenge.ui.repository.detail
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.mydigipay.challenge.base.BaseViewModel
+import com.mydigipay.challenge.data.model.CommitDetail
 import com.mydigipay.challenge.data.model.GitRepo
 import com.mydigipay.challenge.data.model.fillRest
+import com.mydigipay.challenge.data.network.ApiResult
+import com.mydigipay.challenge.data.repository.commit.CommitRepository
+import com.mydigipay.challenge.util.ktx.launch
+import com.mydigipay.challenge.util.livedata.StateLessEvent
 
-class RepositoryDetailViewModel : BaseViewModel() {
+class RepositoryDetailViewModel(
+    private val commitRepository: CommitRepository
+) : BaseViewModel() {
 
     private val _gitRepo = MutableLiveData<GitRepo>()
     val gitRepo: LiveData<GitRepo>
         get() = _gitRepo
 
+    private val _commits = MutableLiveData<List<CommitDetail>>()
+    val commits: LiveData<List<CommitDetail>>
+        get() = _commits
+
     fun setGitRepo(gitRepo: GitRepo) {
-        _gitRepo.postValue(gitRepo.fillRest())
+        _gitRepo.value = gitRepo.fillRest()
+        getCommits(1)
+    }
+
+    private val _onFetchDataFailed = MutableLiveData<StateLessEvent>()
+    val onFetchDataFailed: LiveData<StateLessEvent>
+        get() = _onFetchDataFailed
+
+    fun getCommits(page: Int) = launch {
+        val gitRepo = _gitRepo.value ?: return@launch
+        val commits =  (_commits.value ?: emptyList()).toMutableList()
+        when (val result = commitRepository.getCommits(gitRepo.owner.login, gitRepo.name, page)) {
+            is ApiResult.Success -> {
+                commits.addAll(result.data)
+                _commits.postValue(commits)
+            }
+            is ApiResult.Error -> {
+                showSnackBar(result.message)
+                _onFetchDataFailed.postValue(StateLessEvent())
+            }
+        }
     }
 }
