b06469e53986277e033bdcc899edf21326b5dbe9
TurKurT656 <turkurt656@gmail.com>
2020-03-05 21:52:03 +0330

    added commit repo and func to get commits of git repository

--- app/src/main/java/com/mydigipay/challenge/data/network/api/commit/CommitService.kt
+++ app/src/main/java/com/mydigipay/challenge/data/network/api/commit/CommitService.kt
@@ -3,13 +3,16 @@ package com.mydigipay.challenge.data.network.api.commit
 import com.mydigipay.challenge.data.model.CommitDetail
 import retrofit2.http.GET
 import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface CommitService {
 
     @GET("repos/{owner}/{repo}/commits")
     suspend fun getCommits(
         @Path("owner") owner: String,
-        @Path("repo") repo: String
+        @Path("repo") repo: String,
+        @Query("page") page: Int,
+        @Query("per_page") perPage: Int
     ): List<CommitDetail>
 
 }
