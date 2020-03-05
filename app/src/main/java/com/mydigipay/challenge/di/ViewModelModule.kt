c015690924fc8f70080be10b3974f077ea57c292
TurKurT656 <turkurt656@gmail.com>
2020-03-06 02:21:20 +0330

    - removed splash page - added navigation from repo list to login

--- app/src/main/java/com/mydigipay/challenge/di/ViewModelModule.kt
+++ app/src/main/java/com/mydigipay/challenge/di/ViewModelModule.kt
@@ -4,15 +4,13 @@ import com.mydigipay.challenge.ui.home.HomeViewModel
 import com.mydigipay.challenge.ui.login.LoginViewModel
 import com.mydigipay.challenge.ui.repository.detail.RepositoryDetailViewModel
 import com.mydigipay.challenge.ui.repository.list.RepositoryListViewModel
-import com.mydigipay.challenge.ui.splash.SplashViewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 val viewModelModule = module {
 
     viewModel { LoginViewModel(get()) }
-    viewModel { HomeViewModel() }
-    viewModel { SplashViewModel(get()) }
+    viewModel { HomeViewModel(get()) }
     viewModel { RepositoryListViewModel(get()) }
     viewModel { RepositoryDetailViewModel(get()) }
 
--- app/src/main/java/com/mydigipay/challenge/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/mydigipay/challenge/ui/home/HomeViewModel.kt
@@ -1,12 +1,19 @@
 package com.mydigipay.challenge.ui.home
 
+import com.mydigipay.challenge.R
 import com.mydigipay.challenge.base.BaseViewModel
+import com.mydigipay.challenge.data.repository.token.TokenRepository
+import com.mydigipay.challenge.ui.repository.list.RepositoryListFragmentDirections
+import com.mydigipay.challenge.util.ktx.launch
 
-class HomeViewModel : BaseViewModel() {
+class HomeViewModel(
+    private val tokenRepository: TokenRepository
+) : BaseViewModel() {
 
 
-    fun onProfileClicked() {
-
+    fun onProfileClicked() = launch{
+        if (tokenRepository.isUserLoggedIn()) showSnackBar(R.string.login_successful)
+        else navigateTo(RepositoryListFragmentDirections.actionRepositoriesToLogin())
     }
 
 }
