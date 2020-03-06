11176d02925f9ba635a00a4b56f3e8f07034c315
TurKurT656 <turkurt656@gmail.com>
2020-03-06 19:50:57 +0330

    optimized animations of profile page

--- app/src/main/java/com/mydigipay/challenge/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/mydigipay/challenge/ui/profile/ProfileFragment.kt
@@ -4,11 +4,14 @@ import com.mydigipay.challenge.R
 import com.mydigipay.challenge.base.BaseFragment
 import com.mydigipay.challenge.databinding.FragmentProfileBinding
 import com.mydigipay.challenge.util.FragmentOnBackPressed
+import com.mydigipay.challenge.util.ktx.SwipeState
+import com.mydigipay.challenge.util.ktx.onSwipe
 import kotlinx.android.synthetic.main.fragment_profile.*
 import org.koin.android.ext.android.inject
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
-class ProfileFragment : BaseFragment<ProfileViewModel, FragmentProfileBinding>(), FragmentOnBackPressed {
+class ProfileFragment : BaseFragment<ProfileViewModel, FragmentProfileBinding>(),
+    FragmentOnBackPressed {
 
     override val viewModel: ProfileViewModel by viewModel()
     override val layoutRes: Int = R.layout.fragment_profile
@@ -17,6 +20,12 @@ class ProfileFragment : BaseFragment<ProfileViewModel, FragmentProfileBinding>()
     override fun oneTimeEvent() {
         getHome()?.toggleBottomSection(false)
         info_list.adapter = adapter
+        scroller_view.onSwipe { state ->
+            when (state) {
+                SwipeState.DOWN -> edit_fab.hide()
+                SwipeState.UP -> edit_fab.show()
+            }
+        }
     }
 
     override fun initBinding() {
