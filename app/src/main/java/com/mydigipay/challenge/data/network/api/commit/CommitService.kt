5e23c73b6cd18bdb139ac38ca2fa5cdabbd09b80
TurKurT656 <turkurt656@gmail.com>
2020-03-06 23:26:05 +0330

    made auth state of some calls optional

--- app/src/main/java/com/mydigipay/challenge/data/network/api/commit/CommitService.kt
+++ app/src/main/java/com/mydigipay/challenge/data/network/api/commit/CommitService.kt
@@ -1,13 +1,17 @@
 package com.mydigipay.challenge.data.network.api.commit
 
 import com.mydigipay.challenge.data.model.CommitDetail
+import com.mydigipay.challenge.data.network.interceptor.AUTH_STATE
+import com.mydigipay.challenge.data.network.interceptor.AUTH_STATE_OPTIONAL
 import retrofit2.http.GET
+import retrofit2.http.Headers
 import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface CommitService {
 
     @GET("repos/{owner}/{repo}/commits")
+    @Headers("$AUTH_STATE:$AUTH_STATE_OPTIONAL")
     suspend fun getCommits(
         @Path("owner") owner: String,
         @Path("repo") repo: String,
