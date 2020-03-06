79c959a0ec148c93aa675275bacc1614b235a096
TurKurT656 <turkurt656@gmail.com>
2020-03-06 17:43:35 +0330

    set adapter to profile list

--- app/src/main/java/com/mydigipay/challenge/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/mydigipay/challenge/ui/profile/ProfileFragment.kt
@@ -3,12 +3,20 @@ package com.mydigipay.challenge.ui.profile
 import com.mydigipay.challenge.R
 import com.mydigipay.challenge.base.BaseFragment
 import com.mydigipay.challenge.databinding.FragmentProfileBinding
+import com.mydigipay.challenge.util.FragmentOnBackPressed
+import kotlinx.android.synthetic.main.fragment_profile.*
+import org.koin.android.ext.android.inject
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class ProfileFragment : BaseFragment<ProfileViewModel, FragmentProfileBinding>(), FragmentOnBackPressed {
 
     override val viewModel: ProfileViewModel by viewModel()
     override val layoutRes: Int = R.layout.fragment_profile
+    private val adapter: ProfileItemAdapter by inject()
+
+    override fun oneTimeEvent() {
+        info_list.adapter = adapter
+    }
 
     override fun initBinding() {
         binding?.apply {
