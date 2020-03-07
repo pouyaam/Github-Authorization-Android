03f94d8d48cf9459ee0d72aeeede8eed1e5c8aa7
mo22sen <mohsenghafarpoor93@gmail.com>
2020-03-07 23:39:57 +0330

    calling commits api

--- app/src/main/java/com/mydigipay/challenge/ui/search/detail/DetailModel.kt
+++ app/src/main/java/com/mydigipay/challenge/ui/search/detail/DetailModel.kt
@@ -1,6 +1,13 @@
 package com.mydigipay.challenge.ui.search.detail
 
 import com.mydigipay.challenge.base.BaseModel
+import com.mydigipay.challenge.data.repository.CommitRepository
 
-class DetailModel : BaseModel() {
+class DetailModel(private val commitRepository: CommitRepository) : BaseModel() {
+
+    suspend fun getCommits(
+        owner: String,
+        repo: String,
+        page: Int
+    ) = commitRepository.getCommitsRepository(owner, repo, page)
 }
--- app/src/main/java/com/mydigipay/challenge/ui/search/detail/DetailViewModel.kt
+++ app/src/main/java/com/mydigipay/challenge/ui/search/detail/DetailViewModel.kt
@@ -1,6 +1,42 @@
 package com.mydigipay.challenge.ui.search.detail
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
 import com.mydigipay.challenge.base.BaseViewModel
+import com.mydigipay.challenge.data.network.ApiResult
+import com.mydigipay.challenge.data.pojo.Commit
+import com.mydigipay.challenge.data.pojo.Repository
+import com.mydigipay.challenge.data.pojo.fillRest
+import kotlinx.coroutines.launch
 
-class DetailViewModel(model: DetailModel) : BaseViewModel(model) {
+class DetailViewModel(private val model: DetailModel) : BaseViewModel(model) {
+
+    private val _repository = MutableLiveData<Repository>()
+    val repository: LiveData<Repository>
+        get() = _repository
+
+    private val _commits = MutableLiveData<List<Commit>>()
+    val commits: LiveData<List<Commit>>
+        get() = _commits
+
+    fun setGitRepo(repository: Repository) {
+        _repository.value = repository.fillRest()
+        getCommits(1)
+    }
+
+    fun getCommits(page: Int) = viewModelScope.launch {
+        val repository = _repository.value ?: return@launch
+        val commits = (_commits.value ?: emptyList()).toMutableList()
+        when (val result = model.getCommits(repository.owner.login, repository.name, page)) {
+            is ApiResult.Success -> {
+                commits.addAll(result.data)
+                _commits.postValue(commits)
+            }
+            is ApiResult.Error -> {
+            }
+
+        }
+    }
+
 }
