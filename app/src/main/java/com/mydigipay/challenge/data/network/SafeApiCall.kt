848256402b203b83d1c97aa762b3efb4cbeeace8
TurKurT656 <turkurt656@gmail.com>
2020-03-07 03:56:54 +0330

    changed error handling logic

--- app/src/main/java/com/mydigipay/challenge/data/network/SafeApiCall.kt
+++ app/src/main/java/com/mydigipay/challenge/data/network/SafeApiCall.kt
@@ -1,28 +1,16 @@
 package com.mydigipay.challenge.data.network
 
+import com.mydigipay.challenge.data.network.error.ApiException
+import com.mydigipay.challenge.data.network.error.handleError
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
-import okio.IOException
-import retrofit2.HttpException
-import java.net.SocketTimeoutException
 
 
 sealed class ApiResult<out T : Any> {
 
     data class Success<out T : Any>(val data: T) : ApiResult<T>()
-    data class Error(val message: String, val errorType: ErrorType) : ApiResult<Nothing>()
+    data class Error(val error: ApiException) : ApiResult<Nothing>()
 
-}
-
-enum class ErrorType {
-    TIMEOUT, NETWORK, CUSTOM, UNKNOWN
-}
-
-private fun getErrorType(e: Exception) = when (e) {
-    is HttpException -> ErrorType.CUSTOM
-    is IOException -> ErrorType.NETWORK
-    is SocketTimeoutException -> ErrorType.TIMEOUT
-    else -> ErrorType.UNKNOWN
 }
 
 suspend fun <T : Any> safeApiCall(
@@ -33,7 +21,6 @@ suspend fun <T : Any> safeApiCall(
             call()
         } catch (e: Exception) {
             e.printStackTrace()
-            val message = "Unknown Error" /* TODO: parse from response or get from function */
-            ApiResult.Error(message, getErrorType(e))
+            ApiResult.Error(handleError(e))
         }
     }
--- app/src/main/java/com/mydigipay/challenge/data/network/error/ApiException.kt
+++ app/src/main/java/com/mydigipay/challenge/data/network/error/ApiException.kt
@@ -2,4 +2,12 @@ package com.mydigipay.challenge.data.network.error
 
 import okio.IOException
 
-data class ApiException(override val message: String) : IOException()
+data class ApiException(
+    override val message: String,
+    val type: ErrorType,
+    val code: Int? = null
+) : IOException()
+
+enum class ErrorType {
+    TIMEOUT, NETWORK, CLIENT, SERVER, UNKNOWN
+}
