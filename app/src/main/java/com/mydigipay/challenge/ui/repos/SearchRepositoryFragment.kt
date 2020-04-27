174bf3d077d0e01505275b95e5e12498cecb4458
Saeed Darvish <sd.saeed.darvish@gmail.com>
2020-04-27 16:22:21 +0430

    Redesign item repo

--- app/src/main/java/com/mydigipay/challenge/ui/repos/SearchRepositoryFragment.kt
+++ app/src/main/java/com/mydigipay/challenge/ui/repos/SearchRepositoryFragment.kt
@@ -1,13 +1,12 @@
 package com.mydigipay.challenge.ui.repos
 
-import android.os.Bundle
-import android.view.View
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.mydigipay.challenge.R
 import com.mydigipay.challenge.base.BaseFragment
 import com.mydigipay.challenge.databinding.FragmentSearchRepositoriesBinding
 import com.mydigipay.challenge.ui.adapters.RepoAdapter
 import com.mydigipay.challenge.util.EndlessRecyclerViewScrollListener
+import com.mydigipay.challenge.util.EqualSpacingItemDecoration
 import com.mydigipay.challenge.util.ext.autoComplete
 import org.koin.android.ext.android.inject
 
@@ -28,22 +27,25 @@ class SearchRepositoryFragment :
         }
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        configList()
-        viewModel.query.value = "and"
-    }
 
     private fun configList() {
         binding.repoListRv.apply {
             layoutManager = LinearLayoutManager(context)
             adapter = repoAdapter
+
             addOnScrollListener(object :
                 EndlessRecyclerViewScrollListener(layoutManager as LinearLayoutManager) {
                 override fun onLoadMore(page: Int, totalItemsCount: Int) {
                     viewModel.search(page)
                 }
             })
+
+            addItemDecoration(
+                EqualSpacingItemDecoration(
+                    resources.getDimensionPixelSize(R.dimen.viewHolderHalfMargin),
+                    EqualSpacingItemDecoration.VERTICAL
+                )
+            )
         }
     }
 }
