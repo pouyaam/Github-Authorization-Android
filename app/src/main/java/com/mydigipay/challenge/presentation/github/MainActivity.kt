7d6b35351699f86f2c31f1bb2438342b522f1636
Mohsen Einhesari <mohseneinhesari@yahoo.com>
2020-07-23 18:51:23 +0430

    fix navigation graph bug

--- app/src/main/java/com/mydigipay/challenge/presentation/github/MainActivity.kt
+++ app/src/main/java/com/mydigipay/challenge/presentation/github/MainActivity.kt
@@ -1,9 +1,9 @@
 package com.mydigipay.challenge.presentation.github
 
-import android.app.Activity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : Activity() {
+class MainActivity : AppCompatActivity() {
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
